5846150549bf880115e9fe641e2667f7740190ab
Elmer Balbin Jr <contact@elmer.dev>
2023-03-13 10:40:51 -0700

    queries and css updates to global

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import '@/styles/reset/normalize.css'
 import '@/styles/globals.css'
+import { ApolloProvider } from '@apollo/client'
+import client from '@/apollo/client'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Head>
+        <title>Fine Arts 2023</title>
+        <meta name="description" content="Fine Arts 2023" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
