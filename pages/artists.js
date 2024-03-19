8fa3e329b50810c6d3f6bec85904812fb0beaf93
Wonnnyo Hamester <wonnyohamester@outlook.com>
2024-03-18 17:05:54 -0700

    fix: splash screen path

--- pages/artists.js
+++ pages/artists.js
@@ -1,95 +1,90 @@
 import client from "../apollo/client"
 import { GET_ARTISTS, GET_ARTIST_LIST } from "../apollo/queries/queries"
-import { studioArray } from '../config/data_config'
+import { imageBasePath, studioArray } from "../config/data_config"
 import styles from "../styles/Artists.module.css"
 import ArtworkContainer from "../components/ArtworksContainer"
-import Link from 'next/link'
-import React, { useState, useEffect } from 'react';
-import Header from '@/components/Header'
+import Link from "next/link"
+import React, { useState, useEffect } from "react"
+import Header from "@/components/Header"
 import Footer from "@/components/Footer"
 
+import Image from "next/image"
 
-import Image from 'next/image';
+const Artists = ({ aList }) => {
+  const [artistList, setArtistList] = useState({})
+  const [artist, setArtist] = useState({})
+  const [vw, setVw] = useState(1)
+  const [scrollPosition, setScrollPosition] = useState(0)
+  const [headerStyle, setHeaderStyle] = useState("transparent")
+  const [headerOrigin, setHeaderOrigin] = useState("artists")
 
+  useEffect(() => {
+    let alist = getArtistList()
+    let a = getArtists()
+    a.then((result) => {
+      setArtist(result)
+    })
+    alist.then((result) => {
+      setArtistList(result)
+    })
 
+    const handleScroll = (event) => {
+      const width = window.innerWidth * 0.01
+      const position = window.pageYOffset
 
+      setVw(width)
+      setScrollPosition(position)
+    }
 
+    window.addEventListener("scroll", handleScroll)
 
-const Artists = ({aList})=>{
-    const [artistList, setArtistList] = useState({})
-    const [artist, setArtist] = useState({})
-    const [vw, setVw] = useState(1)
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const [headerStyle, setHeaderStyle] = useState('transparent')
-    const [headerOrigin, setHeaderOrigin] = useState('artists')
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
+  useEffect(() => {
+    if (scrollPosition >= 35 * vw) {
+      setHeaderOrigin("about")
+    } else {
+      setHeaderOrigin("artists")
+    }
+  }, [scrollPosition])
 
-    useEffect(()=>{
-        let alist = getArtistList();
-        let a = getArtists();
-        a.then(result=>{
-            setArtist(result)
-        })
-        alist.then(result=>{
-            setArtistList(result)
-        })
+  return (
+    <div>
+      {/* header */}
+      <div
+        className={styles.artistsHeader}
+        style={{ backgroundColor: headerStyle }}
+      >
+        {artistList?.length > 0 && (
+          <Header
+            artistList={aList}
+            studioList={studioArray}
+            originPage={headerOrigin}
+            bgColor={headerStyle}
+          />
+        )}
+      </div>
 
+      <div className={styles.heroSection}>
+        <Image
+          src={`${imageBasePath}/images/NewArtistsPage1.jpg`}
+          alt="cover image"
+          layout="fill"
+          objectFit="cover"
+          objectPosition="bottom right"
+        />
+        <h1> ARTISTS</h1>
+      </div>
 
-
-        const handleScroll = (event) => {
-
-            const width = window.innerWidth * 0.01;
-            const position = window.pageYOffset;
-
-            setVw(width);
-            setScrollPosition(position)
-        };
-    
-        
-        window.addEventListener("scroll", handleScroll)
-
-
-        return()=>{window.removeEventListener('scroll',handleScroll)}
-
-    },[])
-
-    useEffect(()=>{
-
-        if(scrollPosition >= 35*vw){
-            setHeaderOrigin('about')
-        }else{
-            setHeaderOrigin('artists')
-        }
-    },[scrollPosition])
-
-
-    return(
-
-        <div>
-        {/* header */}
-        <div className={styles.artistsHeader} style={{ backgroundColor: headerStyle }}>
-
-            {(artistList?.length>0)&&<Header artistList={aList} studioList={studioArray} originPage={headerOrigin} bgColor={headerStyle}/>}
-        </div>
-
-
-        <div className={styles.heroSection}>
-            <Image
-                src="/images/NewArtistsPage1.jpg"
-                alt="cover image"
-                layout="fill"
-                objectFit="cover"
-                objectPosition="bottom right"
-            />
-            <h1 > ARTISTS</h1>
-        </div>
-
-        <div>
-            <ArtworkContainer items = {artist} originPage='artists'/>
-        </div>
-            <Footer/>
-        </div>
-    )
+      <div>
+        <ArtworkContainer items={artist} originPage="artists" />
+      </div>
+      <Footer />
+    </div>
+  )
 }
 
 export default Artists
